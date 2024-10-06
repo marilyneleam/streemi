870e01e6e3ee5d27dc4a1c993f1a36dd30777f96
marilyne.leam <marilyne.leam@eemi.com>
2024-10-07 01:45:24 +0200

    add entity

---
+++ src/Entity/Season.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SeasonRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SeasonRepository::class)]
+class Season
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
